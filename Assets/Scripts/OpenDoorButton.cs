5980a71c6577a1d7ee2b3796b50d3cf356d53d9c
wtycjan <56653553+wtycjan@users.noreply.github.com>
2020-08-12 20:55:16 +0200

    initial repo

---
+++ Assets/Scripts/OpenDoorButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorButton : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    private Animator btnAnim;
+    private Animator doorAnim;
+    [SerializeField] private bool open = false;
+    void Start()
+    {
+        btnAnim = GetComponent<Animator>();
+        doorAnim = door.GetComponent<Animator>();
+    }
+
+    public void Interact()
+    {
+        if(open==false)
+            StartCoroutine("OpenDoor");
+        else
+            StartCoroutine("CloseDoor");
+    }
+    public IEnumerator OpenDoor()
+    {
+        btnAnim.SetBool("IsPressed", true);
+        doorAnim.SetBool("IsOpen", true);
+        yield return new WaitForSeconds(.1f);
+        btnAnim.SetBool("IsPressed", false);
+        yield return new WaitForSeconds(6f);
+        open = true;
+    }
+    public IEnumerator CloseDoor()
+    {
+        open = false;
+        btnAnim.SetBool("IsPressed", true);
+        doorAnim.SetBool("IsOpen", false);
+        yield return new WaitForSeconds(.1f);
+        btnAnim.SetBool("IsPressed", false);
+        yield return new WaitForSeconds(6f);
+        open = false;
+    }
+}
