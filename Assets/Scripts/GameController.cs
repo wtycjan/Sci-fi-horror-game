c2c4bea64a34cc167d120787ebe7204a063c087b
wtycjan <56653553+wtycjan@users.noreply.github.com>
2020-08-13 19:24:10 +0200

    demo map

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject door1;
+    [SerializeField] private GameObject door2;
+    [SerializeField] private GameObject door3;
+    [SerializeField] private GameObject door4;
+
+
+
+    void OpenDoor1()
+    {
+        door1.SendMessage("Interact");
+    }
+    void OpenDoor2()
+    {
+        door2.SendMessage("Interact");
+    }
+    void OpenDoor3()
+    {
+        door3.SendMessage("Interact");
+    }
+    void OpenDoor4()
+    {
+        door4.SendMessage("Interact");
+    }
+
+}
---
+++ Assets/Scripts/NetworkClientUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class NetworkClientUI : MonoBehaviour
+{
+    NetworkClient client;
+    private void OnGUI()
+    {
+        string ipaddress = LocalIPAddress();
+        GUI.Box(new Rect(10, Screen.height - 50, 100, 50), ipaddress);
+        GUI.Label(new Rect(20, Screen.height - 30, 100, 20), "Status:" + client.isConnected);
+
+        if(!client.isConnected)
+        {
+            if(GUI.Button(new Rect(10,10,60,50),"Connect"))
+            {
+                Connect();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = new NetworkClient();
+    }
+
+    private void Connect()
+    {
+        client.Connect("192.168.0.13", 25000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public string LocalIPAddress()
+    {
+        IPHostEntry host;
+        string localIP = "";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+    }
+}
