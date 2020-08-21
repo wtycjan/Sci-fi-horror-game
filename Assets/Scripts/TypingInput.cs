a71dd8cc34dfd01a9259f2575168fc9d67c714c6
wtycjan <56653553+wtycjan@users.noreply.github.com>
2020-08-21 20:59:18 +0200

    hacking pc improved

--- Assets/Scripts/TypingInput.cs
+++ Assets/Scripts/TypingInput.cs
@@ -7,22 +7,25 @@ using System;
 using System.Text;
 public class TypingInput : MonoBehaviour
 {
-    InputField myField;
+    public InputField myField;
     public Text osText;
     public Chest chest;
     public NetworkServerUI network;
     private Dictionary<string, System.Action<string, string>> commands;
+    public int securityLvl;
+    private string resetText;
+    private Vector3 inputPosition;
     void Start()
     {
-        myField = GetComponent<InputField>();
         commands = new Dictionary<string, System.Action<string, string>>();
-        // Add the commands you want to recognise along with the functions to call
-        commands.Add("passcode.Override();", User);
-        commands.Add("firewall.Bypass();", Firewall);
-        //commands.Add("user.GrantAccess();", User);
-        commands.Add("mainframe.DisableAlgorithms();", Mainframe);
-        commands.Add("kernel.RequestAccess();", Kernel);
-        commands.Add(GameData.password1, UnlockChest);
+        
+        if(securityLvl>0)
+            commands.Add("system.Override();", Enter);
+        else
+            commands.Add(GameData.password1, UnlockChest);
+
+        resetText = osText.text;
+        inputPosition = myField.gameObject.transform.position;
         // Listen when the inputfield is validated
         myField.onEndEdit.AddListener(OnEndEdit);
 
@@ -30,7 +33,7 @@ public class TypingInput : MonoBehaviour
 
     private void Update()
     {
-        if (gameObject.activeSelf)
+        if (myField.isActiveAndEnabled)
             StartTyping();
     }
     private void OnEndEdit(string input)
@@ -44,7 +47,7 @@ public class TypingInput : MonoBehaviour
         // Find the command
         foreach (var item in commands)
         {
-            if (item.Key.ToLower().StartsWith(input.ToLower()))
+            if (item.Key.Equals(input)) 
             {
                 commandFound = true;
                 item.Value(item.Key, input);
@@ -53,33 +56,60 @@ public class TypingInput : MonoBehaviour
         }
 
         // Do something if the command has not been found
-        if (!commandFound)
-            Debug.Log("No command found");
+        if (!commandFound && securityLvl>0)
+        {
+            Debug.Log("Alarm");
+            osText.text = resetText;
+            myField.gameObject.transform.position = inputPosition;
+            commands.Clear();
+            commands.Add("system.Override();", Enter);
+        }
+            
 
         // Clear the input field (if you want)
         myField.text = "";
     }
+    private void Enter(string command, string input)
+    {
+        osText.text += "\nAccess granted. Security Level: "+ securityLvl;
+        switch (securityLvl)
+        {
+            case 1:
+                osText.text += "\n\nKernel:/ > ";
+                myField.gameObject.transform.position += new Vector3(95, -64, 0);
+                commands.Add("kernel.Disable();", Kernel);
+                break;
+            case 2:
+                osText.text += "\n\nKernel:/Mainframe:/ > ";
+                myField.gameObject.transform.position += new Vector3(205, -64, 0);
+                commands.Add("mainframe.Disable();", Mainframe);
+                break;
+            case 3:
+                osText.text += "\n\nKernel:/Mainframe:/Firewall:/ > ";
+                myField.gameObject.transform.position += new Vector3(295, -64, 0);
+                commands.Add("firewall.Disable();", Firewall);
+                break;
+        }
 
+        
+        Debug.Log("user");
+    }
     private void Firewall(string command, string input)
     {
         osText.text += "\nFirewall has been bypassed.\n\nKernel:/Mainframe:/ > ";
-        myField.gameObject.transform.position += new Vector3(-83, -75, 0);
-    }
-    private void User(string command, string input)
-    {
-        osText.text += "\nUser logged in.\n\nKernel:/Mainframe:/Firewall:/ > ";
-        myField.gameObject.transform.position += new Vector3(300, -65, 0);
-        Debug.Log("user");
+        myField.gameObject.transform.position += new Vector3(-88, -74, 0);
+        commands.Add("mainframe.Disable();", Mainframe);
     }
 
     private void Mainframe(string command, string input)
     {
         osText.text += "\nMainframe algorithms disabled.\n\nKernel:/ > ";
-        myField.gameObject.transform.position += new Vector3(-120, -75, 0);
+        myField.gameObject.transform.position += new Vector3(-110, -74, 0);
+        commands.Add("kernel.Disable();", Kernel);
     }
     private void Kernel(string command, string input)
     {
-        osText.text += "\nAccess granted.\n\tPasscode: "+ GameData.password1;
+        osText.text += "\nKernel breached.\n\tPasscode: "+ GameData.password1;
         myField.gameObject.SetActive(false);
         network.ServerSendMessage("Pswd "+ GameData.password1);
     }
