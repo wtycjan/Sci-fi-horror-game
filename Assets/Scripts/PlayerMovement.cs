17fe4826ee1a960e7354247807ffee3195235450
wtycjan <56653553+wtycjan@users.noreply.github.com>
2020-08-24 13:05:53 +0200

    peeking + sprint

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,21 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight = 5f;
 
     public Transform groundCheck;
+    public Transform cameraControl;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    private Sounds sounds;
     Vector3 velocity;
     bool isGrounded;
+    bool stop = false;
+    bool sprint = false;
 
+    private void Start()
+    {
+        sounds = GetComponentInChildren<Sounds>();
+        StartCoroutine(Footsteps());
+    }
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -31,16 +40,82 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (!stop)
+            controller.Move(move * speed * Time.deltaTime);
 
-        if (Input.GetButton("Jump") && isGrounded)
+
+        //jump removed
+        /*if (Input.GetButton("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        }*/
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            stop = true;
+            if (cameraControl.localPosition.x>-1f)
+            {
+                cameraControl.Rotate(new Vector3(0, 0, 30) * Time.deltaTime);
+                cameraControl.localPosition = new Vector3(cameraControl.localPosition.x - 3.5f * Time.deltaTime, cameraControl.localPosition.y, cameraControl.localPosition.z);
+            }
+
+        }
+        else if (!Input.GetKey(KeyCode.Q) && cameraControl.localPosition.x < 0)
+        {
+            stop = false;
+            cameraControl.Rotate(new Vector3(0, 0, -30) * Time.deltaTime);
+            cameraControl.localPosition = new Vector3(cameraControl.localPosition.x + 3.5f * Time.deltaTime, cameraControl.localPosition.y, cameraControl.localPosition.z);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            stop = true;
+            if (cameraControl.localPosition.x < 1f)
+            {
+                cameraControl.Rotate(new Vector3(0, 0, -30) * Time.deltaTime);
+                cameraControl.localPosition = new Vector3(cameraControl.localPosition.x + 3.5f * Time.deltaTime, cameraControl.localPosition.y, cameraControl.localPosition.z);
+            }
+        }
+        else if (!Input.GetKey(KeyCode.E) && cameraControl.localPosition.x > 0)
+        {
+            stop = false;
+            cameraControl.Rotate(new Vector3(0, 0, 30) * Time.deltaTime);
+            cameraControl.localPosition = new Vector3(cameraControl.localPosition.x - 3.5f * Time.deltaTime, cameraControl.localPosition.y, cameraControl.localPosition.z);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && !sprint)
+        {
+            sprint = true;
+            speed =6;
+        }
+        else if(!Input.GetKey(KeyCode.LeftShift) && sprint)
+        {
+            sprint = false;
+            speed =2.5f;
+        }
+    }
+
+    private IEnumerator Footsteps()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            if (sprint)
+            {
+                sounds.SoundRandom(1);
+            }
+            else
+            {
+                sounds.SoundRandom(.55f);
+                yield return new WaitForSeconds(.5f);
+                
+            }
+        }
+        yield return new WaitForSeconds(.35f);
+        StartCoroutine(Footsteps());
     }
 }
--- Assets/Scripts/Sounds.cs
+++ Assets/Scripts/Sounds.cs
@@ -8,6 +8,7 @@ public class Sounds : MonoBehaviour
     public AudioClip sound2;
     public AudioClip sound3;
     public AudioClip sound4;
+    public AudioClip sound5;
     private AudioSource audioSource;
     void Start()
     {
@@ -23,9 +24,48 @@ public class Sounds : MonoBehaviour
     {
         audioSource.PlayOneShot(sound2);
     }
+    public void Sound3()
+    {
+        audioSource.PlayOneShot(sound2);
+    }
+    public void Sound4()
+    {
+        audioSource.PlayOneShot(sound2);
+    }
+    public void Sound5()
+    {
+        audioSource.PlayOneShot(sound2);
+    }
+    public void SoundRandom(float y)
+    {
+        int x = Random.Range(1, 6);
+        switch (x)
+        {
+            case 1:
+                audioSource.PlayOneShot(sound1,y);
+                break;
+            case 2:
+                audioSource.PlayOneShot(sound2, y);
+                break;
+            case 3:
+                audioSource.PlayOneShot(sound3, y);
+                break;
+            case 4:
+                audioSource.PlayOneShot(sound4, y);
+                break;
+            case 5:
+                audioSource.PlayOneShot(sound5, y);
+                break;
+        }
+        
+    }
     public void Stop()
     {
         audioSource.loop = false;
         audioSource.Stop();
+    }
+    public void Volume(float x)
+    {
+        audioSource.volume = x;
     }
 }
