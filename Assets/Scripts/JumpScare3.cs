f9bf2dfa274d2550612a82f05285a48c5640f727
wtycjan <56653553+wtycjan@users.noreply.github.com>
2020-11-16 20:40:12 +0100

    Fix minigames

---
+++ Assets/Scripts/JumpScare3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScare3 : MonoBehaviour
+{
+    public GameObject spider1;
+    public GameObject spider2;
+    private Sounds sound;
+    bool jumpscare = false;
+    private void Start()
+    {
+        sound = GameObject.FindGameObjectWithTag("SoundController").GetComponent<Sounds>();
+    }
+    public void Interact()
+    {
+        if (!jumpscare)
+        {
+            spider1.SetActive(true);
+            spider2.SetActive(true);
+            sound.Sound3();
+            jumpscare = true;
+        }
+    }
+}
--- Assets/Scripts/Minigames/Keycard.cs
+++ Assets/Scripts/Minigames/Keycard.cs
@@ -4,24 +4,11 @@ using UnityEngine;
 
 public class Keycard : MonoBehaviour
 {
-    public bool jumpscare = true;
-    public GameObject spider1;
-    public GameObject spider2;
-    private Sounds sound;
 
-    private void Start()
-    {
-        sound = GameObject.FindGameObjectWithTag("SoundController").GetComponent<Sounds>();
-    }
+
     public void Interact()
     {
         GameData.level1 = true;
-        if(jumpscare)
-        {
-            spider1.SetActive(true);
-            spider2.SetActive(true);
-            sound.Sound3();
-        }
         Destroy(gameObject);
     }
 
