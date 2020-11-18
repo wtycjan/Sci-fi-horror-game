6ff78cf815831e450d8f5df24c89cdb01480ac06
Esiu <grzegorzzelek10@gmail.com>
2020-11-18 12:43:42 +0100

    Add navmesh lvl2

--- Assets/Scripts/Monster/OpenDoorHandler.cs
+++ Assets/Scripts/Monster/OpenDoorHandler.cs
@@ -5,12 +5,12 @@ using UnityEngine.AI;
 
 public class OpenDoorHandler : MonoBehaviour
 {
-    [SerializeField] private GameObject door1;
-    [SerializeField] private GameObject door2;
-    [SerializeField] private GameObject door3;
-    [SerializeField] private GameObject door4;
-    [SerializeField] private GameObject door5;
-    [SerializeField] private GameObject door6;
+    //[SerializeField] private GameObject door1;
+    //[SerializeField] private GameObject door2;
+    //[SerializeField] private GameObject door3;
+    //[SerializeField] private GameObject door4;
+    //[SerializeField] private GameObject door5;
+    //[SerializeField] private GameObject door6;
     public bool isDoorCloseInFronfOfMonster = false;
 
  
